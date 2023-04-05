5435be5b35a6ffeebfa151e2d44d6223874eaf52
JishithaJacob <jishithavjacob@gmail.com>
2023-04-05 14:19:16 +0100

    first commit

---
+++ src/main/java/io/project/admindomainservice/AdminDomainServiceApplication.java
@@ -0,0 +1,13 @@
+package io.project.admindomainservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminDomainServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminDomainServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/project/admindomainservice/AdminDomainServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.project.admindomainservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminDomainServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
