a6e9e6a7de6169eede7ad0dfb62b20b7fadc9f7c
JishithaJacob <jishithavjacob@gmail.com>
2023-07-13 23:40:07 +0100

    modifications

--- src/main/java/io/project/admindomainservice/controller/HomeController.java
+++ src/main/java/io/project/admindomainservice/controller/HomeController.java
@@ -1,12 +1,16 @@
 package io.project.admindomainservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
-
+@RestController
 public class HomeController {
+
     @GetMapping("/home")
     public String home() {
         return "Hello world";
     }
+
 }
+
+
