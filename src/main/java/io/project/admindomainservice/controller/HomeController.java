ab4fdcee209f074c5f7603750c78f155f00963ce
JishithaJacob <jishithavjacob@gmail.com>
2023-07-13 13:49:29 +0100

    modification in API

--- src/main/java/io/project/admindomainservice/controller/HomeController.java
+++ src/main/java/io/project/admindomainservice/controller/HomeController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.security.Principal;
 
 public class HomeController {
-    @GetMapping("/dashboard")
-    public String home(Principal principal) {
+    @GetMapping("/home")
+    public String home() {
         return "Hello world";
     }
 }
